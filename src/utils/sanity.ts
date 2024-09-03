13f97151e8f672ecc6e1d9092a3f259924323538
Otabek <otabekjon2002@mail.ru>
2024-09-03 21:41:28 +0500

    sanity configiration

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "@sanity/client";
 
 export const client = createClient({
     projectId: "544nqitn",
-    dataset: "tik-tok-backend",
+    dataset: "tik-tok-sanity",
     apiVersion: '2023-05-03',
     useCdn: false,
     token: process.env.NEXT_PUBLIC_SANITY_SECRET_TOKEN,
