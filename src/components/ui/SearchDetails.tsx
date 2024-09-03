d5c43decb3f5e740de1707b14943f7400b1d44b1
Otabek <otabekjon2002@mail.ru>
2024-09-03 21:58:31 +0500

    sanity configiration

--- src/components/ui/SearchDetails.tsx
+++ src/components/ui/SearchDetails.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { IUser, Video } from "../../../types";
 import { useSearchParams } from "next/navigation";
 import useAuthStore from "../../../store/authStore";
@@ -14,7 +14,6 @@ const SearchDetails = () => {
   const [videos, setVideos] = useState<Video[]>([]);
   const [isAccounts, setIsAccounts] = useState(false);
   const { allUsers } = useAuthStore();
-
   const searchParams = useSearchParams();
   const search = searchParams.get("query");
 
@@ -51,63 +50,65 @@ const SearchDetails = () => {
   }, [search, isAccounts]);
 
   return (
-    <div>
-      <div className="flex gap-10 mb-10 mt-10 py-2 border-b-2 border-gray-200 w-full">
-        <p
-          className={`text-xl font-semibold cursor-pointer ${accountsClass} mt-2`}
-          onClick={() => setIsAccounts(true)}
-        >
-          Accounts
-        </p>
-        <p
-          className={`text-xl font-semibold cursor-pointer ${videosClass} mt-2`}
-          onClick={() => setIsAccounts(false)}
-        >
-          Videos
-        </p>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <div className="flex gap-10 mb-10 mt-10 py-2 border-b-2 border-gray-200 w-full">
+          <p
+            className={`text-xl font-semibold cursor-pointer ${accountsClass} mt-2`}
+            onClick={() => setIsAccounts(true)}
+          >
+            Accounts
+          </p>
+          <p
+            className={`text-xl font-semibold cursor-pointer ${videosClass} mt-2`}
+            onClick={() => setIsAccounts(false)}
+          >
+            Videos
+          </p>
+        </div>
+        {isAccounts ? (
+          <div className="md:mt-16">
+            {searchedAccounts?.length ? (
+              searchedAccounts.map((user: IUser, idx: number) => (
+                <Link key={idx} href={`/profile/${user._id}`}>
+                  <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded border-b-2 border-gray-200">
+                    <div>
+                      <Image
+                        width={50}
+                        height={50}
+                        className="rounded-full"
+                        alt="user-profile"
+                        src={user.image}
+                      />
+                    </div>
+                    <div>
+                      <p className="flex gap-1 items-center text-lg font-bold text-primary">
+                        {user.userName} <GoVerified className="text-blue-400" />
+                      </p>
+                      <p className="capitalize text-gray-400 text-sm">
+                        {user.userName}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              ))
+            ) : (
+              <NoResults text={`No Account Results for "${search}"`} />
+            )}
+          </div>
+        ) : (
+          <div className="md:mt-16 flex flex-wrap gap-6 md:justify-start">
+            {videos?.length ? (
+              videos?.map((post: Video, idx: number) => (
+                <PostCard postDetails={post} key={idx} />
+              ))
+            ) : (
+              <NoResults text={`No Video Results for "${search}"`} />
+            )}
+          </div>
+        )}
       </div>
-      {isAccounts ? (
-        <div className="md:mt-16">
-          {searchedAccounts?.length ? (
-            searchedAccounts.map((user: IUser, idx: number) => (
-              <Link key={idx} href={`/profile/${user._id}`}>
-                <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded border-b-2 border-gray-200">
-                  <div>
-                    <Image
-                      width={50}
-                      height={50}
-                      className="rounded-full"
-                      alt="user-profile"
-                      src={user.image}
-                    />
-                  </div>
-                  <div>
-                    <p className="flex gap-1 items-center text-lg font-bold text-primary">
-                      {user.userName} <GoVerified className="text-blue-400" />
-                    </p>
-                    <p className="capitalize text-gray-400 text-sm">
-                      {user.userName}
-                    </p>
-                  </div>
-                </div>
-              </Link>
-            ))
-          ) : (
-            <NoResults text={`No Account Results for "${search}"`} />
-          )}
-        </div>
-      ) : (
-        <div className="md:mt-16 flex flex-wrap gap-6 md:justify-start">
-          {videos?.length ? (
-            videos?.map((post: Video, idx: number) => (
-              <PostCard postDetails={post} key={idx} />
-            ))
-          ) : (
-            <NoResults text={`No Video Results for "${search}"`} />
-          )}
-        </div>
-      )}
-    </div>
+    </Suspense>
   );
 };
 
