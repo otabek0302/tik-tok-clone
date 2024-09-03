7f277f2b46f1f5e7f6d141ac6b3ddaea01049567
Otabek <otabekjon2002@mail.ru>
2024-09-03 21:34:59 +0500

    changed sidebar

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET,
+    projectId: "544nqitn",
+    dataset: "tik-tok-backend",
     apiVersion: '2023-05-03',
     useCdn: false,
     token: process.env.NEXT_PUBLIC_SANITY_SECRET_TOKEN,
